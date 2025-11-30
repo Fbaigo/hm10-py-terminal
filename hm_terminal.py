9d777e25396a65498f5fff1891f8910bd8bfc9a3
Federico Baigorria <febaigo@yahoo.com.ar>
2025-11-30 20:35:46 -0300

    Including new write and receive methods
    A new send AT command method is added, not a private method in case the
    user needs to send any AT command
    Including query methods for workmode

--- hm_terminal.py
+++ hm_terminal.py
@@ -5,13 +5,20 @@ from enum import Enum
 _HM_LOGGER_NAME = "[HM-10-terminal]"
 _HM_LOGGING_LEVEL = logging.INFO
 
-# All AT commands must be terminated at \r\n 
+# All AT commands must be terminated at \r\n
+# op codes terminated with q are queries
+
 class ATCommands(str, Enum):
     op_at = "AT\r\n"
     op_help = "AT+HELP\r\n"
+    op_imme = "AT+IMME\r\n"
+    op_imme_q = "AT+IMME?\r\n"
 
     def opcode(self) -> bytes:
         return bytes(str.__str__(self), encoding='utf-8')
+    
+    def __str__(self) -> str:
+        return str.__str__(self)
 
 class HMTerminal(object):
     logging.basicConfig(
@@ -43,33 +50,17 @@ class HMTerminal(object):
             self.logger.warning(f"Serial port {self.port} already opened")
 
         return self.serial_if.is_open
+    
+    def send_at_command(self, opcode:str) -> str:
+        if "\r\n" not in opcode:
+            opcode = opcode.replace('\r', '')
+            opcode = opcode.replace('\n', '')
+            opcode = ''.join([opcode, "\r\n"])
+            
+        opcode = bytes(opcode, encoding="utf-8", errors="ignore")
+        self.serial_if.write(opcode)
 
-    def test_device(self) -> bool:
-        """
-            Checks if the HM device replies to AT commands properly
-        """
-        self.serial_if.write(ATCommands.op_at.opcode())
-        data = self.serial_if.readline()
-        status_ok = False
-
-        if data:
-            decoded_data = data.decode('utf-8', errors="ignore")
-            self.logger.info(f"AT sent. Received: {decoded_data}")
-        else:
-            self.logger.error("AT sent. No data received")
-
-        status_ok = True if "OK" in decoded_data else False
-        
-        return status_ok
-
-    def get_supported_cmds(self) -> list:
-        """
-            Send AT+HELP command to get a list of all supported commands
-        """
         replies = []
-        self.serial_if.write(ATCommands.op_help.opcode())
-
-        # the read_until(terminator=b'') method doesn't get the complete device reply so we get one by one
         while True:
             data = self.serial_if.readline()
             replies.append(data)
@@ -85,6 +76,46 @@ class HMTerminal(object):
         msg = ''.join(map(lambda d: d.decode('utf-8', errors='ignore'), replies))
         self.logger.debug(f"Received: {msg}")
 
+        return msg
+    
+    def _parse_reply(self, msg:str) -> tuple:
+        status = None
+        param = None
+        
+        try:
+            status = msg.split('+')[0]
+        except:
+            pass
+
+        try:
+            param = msg.split(':')[1].replace('\r\n', '')
+        except:
+            pass
+    
+        return (status, param)
+
+    def test_device(self) -> bool:
+        """
+            Checks if the HM device replies to AT commands properly
+        """
+        status_ok = False
+        msg = self.send_at_command(ATCommands.op_at)
+
+        if msg:
+            self.logger.info(f"AT sent. Received: {msg}")
+        else:
+            self.logger.error("AT sent. No data received")
+
+        status_ok = True if "OK" in msg else False
+        return status_ok
+
+    def get_supported_cmds(self) -> list:
+        """
+            Send AT+HELP command to get a list of all supported commands
+        """
+
+        msg = self.send_at_command(ATCommands.op_help)
+
         msg_cmds_match = msg.split('\n')
         msg_cmds_match = [row[row.find('AT'):-1].split(' ')[0] for row in msg_cmds_match if 'AT' in row]
         self.supported_cmds = msg_cmds_match
@@ -97,6 +128,11 @@ class HMTerminal(object):
         """
         is_supported = True if opcode in self.supported_cmds else False
         return is_supported
+    
+    def get_work_mode(self):
+        msg = self.send_at_command(ATCommands.op_imme_q)
+        workmode = self._parse_reply(msg)
+        return workmode
     
     def __del__(self):
         if self.serial_if.is_open:
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time, sys
 from hm_terminal import HMTerminal
 
+#['AT', 'AT+RESET', 'AT+VERSION', 'AT+HELP', 'AT+NAME', 'AT+PIN', 'AT+BAUD', 'AT+LADDR', 
+# 'AT+ADDR', 'AT+DEFAULT', 'AT+RENEW', 'AT+STATE', 'AT+PWRM', 'AT+POWE', 'AT+SLEEP', 'AT+ROLE', 
+# 'AT+PARI', 'AT+STOP', 'AT+INQ', 'AT+SHOW', 'AT+CONN', 'AT+IMME', 'AT+START', 'AT+UUID', 'AT+CHAR']
+
 if __name__ == "__main__":
     baudrate = 9600
     hm_terminal = HMTerminal("/dev/ttyUSB0", baudrate)
@@ -10,6 +14,7 @@ if __name__ == "__main__":
         
     if hm_terminal.test_device():
         commands = hm_terminal.get_supported_cmds()
+        #print(commands)
     else:
         sys.exit()
 
